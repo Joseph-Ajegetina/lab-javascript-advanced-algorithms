6212b450f44c5b3027c6d1a0f06ca797d5b3e123
Joseph-Ajegetina <josephajongbah@gmail.com>
2023-06-03 15:24:19 +0000

    Solved lab

--- src/queue/queue-data-structure.js
+++ src/queue/queue-data-structure.js
@@ -6,22 +6,50 @@ class Queue {
 
   canEnqueue() {
     // ... your code goes here
+    return this.queueControl.length < this.MAX_SIZE;
   }
 
   isEmpty() {
     // ... your code goes here
+    return !this.queueControl.length
   }
 
   enqueue(item) {
     // ... your code goes here
+    if(this.canEnqueue()){
+      this.queueControl.push(item);
+      return this.queueControl
+    }else{
+      throw new Error('QUEUE_OVERFLOW')
+    }
   }
 
   dequeue() {
     // ... your code goes here
+    if(this.isEmpty()){
+      throw new Error('QUEUE_UNDERFLOW')
+    }else{
+
+      const item = this.queueControl[0]
+      const update = this.queueControl.filter((elem) => { 
+        
+        if(elem == item){
+          return true
+        }else {
+          return false;
+
+        }
+      })
+  
+      this.queueControl = update;
+  
+      return item
+    }
   }
 
   display() {
     // ... your code goes here
+    return this.queueControl;
   }  
 }
 
